40786254ecfbbd754d4c81e19556594ab1d2fbfe
davepeyton <davepeytonmusic@gmail.com>
2021-06-05 21:37:29 +0000

    add task setup

--- static/js/script.js
+++ static/js/script.js
@@ -4,9 +4,8 @@
 
 $(document).ready(function () {
     $(".sidenav").sidenav({edge: "right"});
+    $(".collapsible").collapsible();
 });
-
-
 
 
 
@@ -17,4 +16,6 @@ $(document).ready(function () {
 // document.addEventListener('DOMContentLoaded', function () {
 //     let sidenavs = document.querySelectorAll(".sidenav");
 //     let sidenavsInstance = M.Sidenav.init(sidenavs, {edge: "right"});
+//     let collapsibles = document.querySelectorAll(".collapsible");
+//     let collapsiblesInstance = M.Collapsible.init(collapsibles);
 // });
